c6b03248a1857629fe7c7c62cbd58c78646cf73d
april-ayres <34463266+april-ayres@users.noreply.github.com>
2017-12-12 14:53:26 +1100

    Add typescript typings for the library

---
+++ index.d.ts
@@ -0,0 +1,102 @@
+// Type definitions for react-native-nfc-manager
+// Project: https://github.com/whitedogg13/react-native-nfc-manager
+// Definitions by: April Ayres <april.ayres@papercut.com> and Paul Huynh <paul.huynh@papercut.com>
+
+declare module 'react-native-nfc-manager' {
+
+	export interface Tag {
+		id?: number[];
+		tnf: number;
+		type: number[];
+		payload: any[];
+	}
+
+	export interface ParseUriResult {
+		uri: string;
+	}
+
+	export interface StartOptions {
+		onSessionClosedIOS(): void;
+	}
+
+	export interface TagEvent {
+		ndefMessage: Tag[];
+		maxSize: number;
+		type: string;
+		techTypes: string[];
+		id: number[];
+	}
+
+	interface NfcManager {
+
+		start(options?: StartOptions): Promise<any>;
+
+		isEnabled(): Promise<boolean>;
+
+		goToNfcSetting(): Promise<any>;
+
+		getLaunchTagEvent(): Promise<Tag>;
+
+		registerTagEvent(listener: (tag: TagEvent) => void): Promise<any>;
+
+		unregisterTagEvent(): Promise<any>;
+
+	}
+	const nfcManager: NfcManager;
+	export default nfcManager;
+
+	export namespace NdefParser {
+		function parseUri(ndef: Tag): ParseUriResult;
+	}
+}// Type definitions for react-native-nfc-manager
+// Project: https://github.com/whitedogg13/react-native-nfc-manager
+// Definitions by: April Ayres <april.ayres@papercut.com> and Paul Huynh <paul.huynh@papercut.com>
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+declare module 'react-native-nfc-manager' {
+
+	export interface Tag {
+		id?: number[];
+		tnf: number;
+		type: number[];
+		payload: any[];
+	}
+
+	export interface ParseUriResult {
+		uri: string;
+	}
+
+	export interface StartOptions {
+		onSessionClosedIOS(): void;
+	}
+
+	export interface TagEvent {
+		ndefMessage: Tag[];
+		maxSize: number;
+		type: string;
+		techTypes: string[];
+		id: number[];
+	}
+
+	interface NfcManager {
+
+		start(options?: StartOptions): Promise<any>;
+
+		isEnabled(): Promise<boolean>;
+
+		goToNfcSetting(): Promise<any>;
+
+		getLaunchTagEvent(): Promise<Tag>;
+
+		registerTagEvent(listener: (tag: TagEvent) => void): Promise<any>;
+
+		unregisterTagEvent(): Promise<any>;
+
+	}
+	const nfcManager: NfcManager;
+	export default nfcManager;
+
+	export namespace NdefParser {
+		function parseUri(ndef: Tag): ParseUriResult;
+	}
+}
