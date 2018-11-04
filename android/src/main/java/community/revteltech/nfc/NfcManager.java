72e3fe5b0ac4de7ee86ad3d8847a0a25189fefc1
Nicolas Van Eenaeme <nicolas@poison.be>
2018-11-04 20:23:29 +0100

    testing explicit mifare support on android

--- android/src/main/java/community/revteltech/nfc/NfcManager.java
+++ android/src/main/java/community/revteltech/nfc/NfcManager.java
@@ -41,6 +41,7 @@ import android.os.Parcelable;
 import org.json.JSONObject;
 import org.json.JSONException;
 
+import java.io.File;
 import java.util.*;
 import java.nio.charset.Charset;
 
@@ -48,6 +49,7 @@ import static android.app.Activity.RESULT_OK;
 import static android.os.Build.VERSION_CODES.LOLLIPOP;
 import static com.facebook.react.bridge.UiThreadUtil.runOnUiThread;
 
+import android.content.pm.FeatureInfo;
 import android.content.pm.PackageManager;
 
 class NfcManager extends ReactContextBaseJavaModule implements ActivityEventListener, LifecycleEventListener {
@@ -695,8 +697,66 @@ class NfcManager extends ReactContextBaseJavaModule implements ActivityEventList
 			return;
 		}
 
-		boolean result = currentActivity.getPackageManager().hasSystemFeature(PackageManager.FEATURE_NFC);
-		callback.invoke(null, result);
+		if (!currentActivity.getPackageManager().hasSystemFeature(PackageManager.FEATURE_NFC)) {
+			callback.invoke(null, false);
+			return;
+		}
+
+		// If we ask for MifareClassic support, so some extra checks
+		// NOTE: since not all chips and devices are compatible with MifareClassic we do need this code
+		if (tech.equals("MifareClassic")) {
+			boolean foundViaFeatures = currentActivity.getPackageManager().hasSystemFeature("com.nxp.mifare");
+			if (!foundViaFeatures) {
+				callback.invoke(null, false);
+				return;
+			}
+
+			// The following code is thanks to ikarus23's MifareClassicTool (https://github.com/ikarus23/MifareClassicTool)
+			// @see https://github.com/ikarus23/MifareClassicTool/blob/3eb37b0afa3f57f0d18f2fcdfcee2435b47886d8/Mifare%20Classic%20Tool/app/src/main/java/de/syss/MifareClassicTool/Common.java#L588
+
+			// Check if there is the NFC device "bcm2079x-i2c".
+			// Chips by Broadcom don't support MIFARE Classic.
+			// This could fail because on a lot of devices apps don't have
+			// the sufficient permissions.
+			// Another exception:
+			// The Lenovo P2 has a device at "/dev/bcm2079x-i2c" but is still
+			// able of reading/writing MIFARE Classic tags. I don't know why...
+			// https://github.com/ikarus23/MifareClassicTool/issues/152
+			boolean isLenovoP2 = Build.MANUFACTURER.equals("LENOVO")
+				&& Build.MODEL.equals("Lenovo P2a42");
+			File device = new File("/dev/bcm2079x-i2c");
+			if (!isLenovoP2 && device.exists()) {
+				callback.invoke(null, false);
+				return;
+			}
+
+			// Check if there is the NFC device "pn544".
+			// The PN544 NFC chip is manufactured by NXP.
+			// Chips by NXP support MIFARE Classic.
+			device = new File("/dev/pn544");
+			if (device.exists()) {
+				callback.invoke(null, true);
+				return;
+			}
+
+			// Check if there are NFC libs with "brcm" in their names.
+			// "brcm" libs are for devices with Broadcom chips. Broadcom chips
+			// don't support MIFARE Classic.
+			File libsFolder = new File("/system/lib");
+			File[] libs = libsFolder.listFiles();
+			for (File lib : libs) {
+				if (lib.isFile()
+					&& lib.getName().startsWith("libnfc")
+					&& lib.getName().contains("brcm")
+					// Add here other non NXP NFC libraries.
+				) {
+					callback.invoke(null, false);
+					return;
+				}
+			}
+		}
+
+		callback.invoke(null, true);
 	}
 
 	@ReactMethod
