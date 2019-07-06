9ce659674162b172e27f8a99b16c3954746e8cfd
Richie <whitedogg13@gmail.com>
2019-07-06 23:11:37 +0800

    support androidx

--- android/src/main/java/community/revteltech/nfc/NfcManager.java
+++ android/src/main/java/community/revteltech/nfc/NfcManager.java
@@ -7,7 +7,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Build;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.Log;
 import android.provider.Settings;
 import com.facebook.react.bridge.*;
