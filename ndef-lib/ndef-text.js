5bfa110d8564e75bc52826abc6bec72f442c899e
Zamfira Costin-Andrei <andrei.zamfira14@yahoo.com>
2019-08-30 21:17:12 +0300

    Update ndef-text.js
    Added UTF-8 decoder

--- ndef-lib/ndef-text.js
+++ ndef-lib/ndef-text.js
@@ -7,17 +7,12 @@ function decode(data) {
     var languageCodeLength = (data[0] & 0x3F), // 6 LSBs
         languageCode = data.slice(1, 1 + languageCodeLength),
         utf16 = (data[0] & 0x80) !== 0; // assuming UTF-16BE
-
-    // TODO need to deal with UTF in the future
-    // console.log("lang " + languageCode + (utf16 ? " utf16" : " utf8"));
     
-    var decodedString = util.bytesToString(data.slice(languageCodeLength + 1));
-    
-    if (typeof decodedString !== 'undefined' && typeof decodedString !== 'null' && strlen(decodedString) > 0) {
-        return decodedString;
+    if (utf16) {
+        return util.bytesToString(data.slice(languageCodeLength + 1));
     }
     
-    decodedString = "";
+    var utf8 = "";
     var i, len, c;
     var char2, char3;
 
@@ -28,23 +23,23 @@ function decode(data) {
         switch(c >> 4)
         {
             case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
-            decodedString += String.fromCharCode(c);
+            utf8 += String.fromCharCode(c);
             break;
             case 12: case 13:
             char2 = data[i++];
-            decodedString += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
+            utf8 += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
             break;
             case 14:
                 char2 = data[i++];
                 char3 = data[i++];
-                decodedString += String.fromCharCode(((c & 0x0F) << 12) |
+                utf8 += String.fromCharCode(((c & 0x0F) << 12) |
                     ((char2 & 0x3F) << 6) |
                     ((char3 & 0x3F) << 0));
                 break;
         }
     }
 
-    return decodedString;
+    return utf8;
 }
 
 // encode text payload
