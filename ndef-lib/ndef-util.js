4c82d1be69f1d14af711b6a53f2c4d3ebe4be28d
Richie <whitedogg13@gmail.com>
2019-09-21 22:01:17 +0800

    fix utf8 bug in ndef-text / ndef-util

--- ndef-lib/ndef-util.js
+++ ndef-lib/ndef-util.js
@@ -2,31 +2,29 @@
 // Copyright 2013 Don Coleman
 //
 
-// Thanks to
-// https://weblog.rogueamoeba.com/2017/02/27/javascript-correctly-converting-a-byte-array-to-a-utf-8-string/
+// https://gist.github.com/joni/3760795
 function _utf8ArrayToStr(data) {
-    const extraByteMap = [1, 1, 1, 1, 2, 2, 3, 0];
-    var count = data.length;
-    var str = "";
+    var str = '',
+        i;
 
-    for (var index = 0; index < count;) {
-        var ch = data[index++];
-        if (ch & 0x80) {
-            var extra = extraByteMap[(ch >> 3) & 0x07];
-            if (!(ch & 0x40) || !extra || ((index + extra) > count))
-                return null;
+    for (i = 0; i < data.length; i++) {
+        var value = data[i];
 
-            ch = ch & (0x3F >> extra);
-            for (; extra > 0; extra -= 1) {
-                var chx = data[index++];
-                if ((chx & 0xC0) != 0x80)
-                    return null;
+        if (value < 0x80) {
+            str += String.fromCharCode(value);
+        } else if (value > 0xBF && value < 0xE0) {
+            str += String.fromCharCode((value & 0x1F) << 6 | data[i + 1] & 0x3F);
+            i += 1;
+        } else if (value > 0xDF && value < 0xF0) {
+            str += String.fromCharCode((value & 0x0F) << 12 | (data[i + 1] & 0x3F) << 6 | data[i + 2] & 0x3F);
+            i += 2;
+        } else {
+            // surrogate pair
+            var charCode = ((value & 0x07) << 18 | (data[i + 1] & 0x3F) << 12 | (data[i + 2] & 0x3F) << 6 | data[i + 3] & 0x3F) - 0x010000;
 
-                ch = (ch << 6) | (chx & 0x3F);
-            }
+            str += String.fromCharCode(charCode >> 10 | 0xD800, charCode & 0x03FF | 0xDC00);
+            i += 3;
         }
-
-        str += String.fromCharCode(ch);
     }
 
     return str;
@@ -34,35 +32,31 @@ function _utf8ArrayToStr(data) {
 
 // https://stackoverflow.com/questions/18729405/how-to-convert-utf8-string-to-byte-array
 function _toUTF8Array(str) {
-    var utf8 = [];
-    for (var i=0; i < str.length; i++) {
-        var charcode = str.charCodeAt(i);
-        if (charcode < 0x80) utf8.push(charcode);
-        else if (charcode < 0x800) {
-            utf8.push(0xc0 | (charcode >> 6),
-                      0x80 | (charcode & 0x3f));
-        }
-        else if (charcode < 0xd800 || charcode >= 0xe000) {
-            utf8.push(0xe0 | (charcode >> 12),
-                      0x80 | ((charcode>>6) & 0x3f),
-                      0x80 | (charcode & 0x3f));
-        }
-        // surrogate pair
-        else {
-            i++;
-            // UTF-16 encodes 0x10000-0x10FFFF by
-            // subtracting 0x10000 and splitting the
-            // 20 bits of 0x0-0xFFFFF into two halves
-            charcode = 0x10000 + (((charcode & 0x3ff)<<10)
-                      | (str.charCodeAt(i) & 0x3ff));
-            utf8.push(0xf0 | (charcode >>18),
-                      0x80 | ((charcode>>12) & 0x3f),
-                      0x80 | ((charcode>>6) & 0x3f),
-                      0x80 | (charcode & 0x3f));
-        }
+  var out = [], p = 0;
+  for (var i = 0; i < str.length; i++) {
+    var c = str.charCodeAt(i);
+    if (c < 128) {
+      out[p++] = c;
+    } else if (c < 2048) {
+      out[p++] = (c >> 6) | 192;
+      out[p++] = (c & 63) | 128;
+    } else if (
+        ((c & 0xFC00) == 0xD800) && (i + 1) < str.length &&
+        ((str.charCodeAt(i + 1) & 0xFC00) == 0xDC00)) {
+      // Surrogate Pair
+      c = 0x10000 + ((c & 0x03FF) << 10) + (str.charCodeAt(++i) & 0x03FF);
+      out[p++] = (c >> 18) | 240;
+      out[p++] = ((c >> 12) & 63) | 128;
+      out[p++] = ((c >> 6) & 63) | 128;
+      out[p++] = (c & 63) | 128;
+    } else {
+      out[p++] = (c >> 12) | 224;
+      out[p++] = ((c >> 6) & 63) | 128;
+      out[p++] = (c & 63) | 128;
     }
-    return utf8;
-}
+  }
+  return out;
+};
 
 function stringToBytes(string) {
     return _toUTF8Array(string);
