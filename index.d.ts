6fc257e61e1da5600a7aac44d9a8a97a7ecc988c
Frans L <jonkun.muun@hotmail.com>
2020-01-04 21:29:23 +0200

    fix: remove getNdefMessage's parameter declaration

--- index.d.ts
+++ index.d.ts
@@ -92,7 +92,7 @@ declare module 'react-native-nfc-manager' {
 
     writeNdefMessage: (bytes: number[]) => Promise<void>;
 
-    getNdefMessage: (bytes: number[]) => Promise<TagEvent | null>;
+    getNdefMessage: () => Promise<TagEvent | null>;
 
     /** [iOS ONLY] */
     setAlertMessageIOS: (alertMessage: string) => Promise<void>;
