13715334ea908b89654b42eba4ec902bcc6f4229
Frans L <jonkun.muun@hotmail.com>
2020-01-04 21:32:54 +0200

    fix: add callback type null possible

--- index.d.ts
+++ index.d.ts
@@ -82,7 +82,7 @@ declare module 'react-native-nfc-manager' {
 
     unregisterTagEvent(): Promise<void>;
 
-    setEventListener(name: NfcEvents, callback: OnNfcEvents): void;
+    setEventListener(name: NfcEvents, callback: OnNfcEvents | null): void;
 
     requestTechnology: (tech: NfcTech) => Promise<NfcTech>;
 
