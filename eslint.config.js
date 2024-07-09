786d5361f24a1ff78ca43b6547b4137201c4ee92
Frederique Lemieux <fredlemi@gmail.com>
2024-07-09 16:53:40 +0200

    Add esling.config.js file and move rules and ignore patterns in to this file

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+module.exports = [
+  {
+    rules: {
+      "no-bitwise": 0,
+    },
+    ignores: [
+      "node_modules/",
+      "coverage/",
+      "dist/",
+      "lib/",
+      "example/",
+      "index.d.ts",
+
+    ],
+  },
+];
